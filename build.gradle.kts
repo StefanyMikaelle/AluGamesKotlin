7fa093b3b1bd7e6be73424eb2c0f22f056dbefaf
StefanyMikaelle <stefany_mikaelle@hotmail.com>
2024-04-13 17:11:14 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation ("org.jetbrains.kotlin:kotlin-test-junit5:1.5.31")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
